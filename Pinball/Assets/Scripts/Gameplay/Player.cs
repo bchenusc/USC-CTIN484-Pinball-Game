cd4ccd17bbc5b2c914b28f9d3db999cfb1158091
Fluffysheep <brianych@usc.edu>
2014-08-27 16:34:36 -0700

    Initial Commit

---
+++ Pinball/Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public enum PlayerType {
+		PLAYER1,
+		PLAYER2
+	}
+
+	public PlayerType myPlayer = PlayerType.PLAYER1;
+
+	public KeyCode left_control = KeyCode.Alpha1;
+	public KeyCode right_control = KeyCode.Alpha2;
+
+
+
+}
