38897d86b89bf66f2550b906e66569ddd1e930c2
Fluffysheep <brianych@usc.edu>
2014-08-27 16:33:52 -0700

    Purge project

--- Pinball/Assets/Scripts/Globals/Delegates.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-	public delegate void Action();
-	public delegate void ActionInt(int i);
-	public delegate void ActionString(string i);
-	public delegate void ActionKey(KeyCode key);
