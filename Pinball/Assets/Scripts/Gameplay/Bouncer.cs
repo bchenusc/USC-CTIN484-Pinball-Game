5bf5eff34384699d328d4b58af909bfcf31d4c7b
Mr Fluffy Sheep <brianych@usc.edu>
2014-08-29 23:31:52 -0700

    fixed some going through paddle problems

--- Pinball/Assets/Scripts/Gameplay/Bouncer.cs
+++ Pinball/Assets/Scripts/Gameplay/Bouncer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Bouncer : MonoBehaviour {
 
-	float force = 750f;
+	float force = 1000f;
 
 	void OnCollisionEnter2D(Collision2D c) {
 		if (c.gameObject.CompareTag("Ball")) {
