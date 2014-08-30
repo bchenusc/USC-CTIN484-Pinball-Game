c9388c741f238989b6f0f79d91261545758c5c46
Mr Fluffy Sheep <brianych@usc.edu>
2014-08-29 23:59:41 -0700

    Added a toggle booster

---
+++ Pinball/Assets/Scripts/Gameplay/ToggleBooster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleBooster : MonoBehaviour {
+
+	bool isEnabled = true;
+
+	void Start() {
+		isEnabled = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if (isEnabled && c.gameObject.CompareTag("Ball")) {
+			Rigidbody2D otherBody = c.gameObject.rigidbody2D;
+			otherBody.velocity = Vector2.zero;
+			otherBody.angularVelocity = 0;
+			otherBody.AddForce(transform.up * Random.Range(2000, 3000));
+			// TODO: Change color here
+
+			isEnabled = false;
+			SingletonObject.Get.getTimer().Add(gameObject.GetInstanceID() + "toogleboosterOn", ToggleBooster, 30);
+		}
+	}
+
+	void ToggleBoosterOn() {
+		isEnabled = true;
+	}
+
+}
